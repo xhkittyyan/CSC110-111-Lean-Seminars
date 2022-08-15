525dc4adde9c9046f435e5d4664fb7b9c0b18845
xhkittyyan <xh.yan@utoronto.ca>
2022-08-15 18:30:38 -0400

    left-associative
    I am not sure if it is accurate to say "Since multiplication is left-associative,.." Please leave comments.

--- src/1_rw/ex1_rw_mul_comm_assoc.lean
+++ src/1_rw/ex1_rw_mul_comm_assoc.lean
@@ -10,12 +10,13 @@ begin
   rw mul_comm a b, 
   /- mul_comm a b refers to the communicative law for multiplication 
   on a and b: a * b = b * a. 'rw mul_comm a b' tells Lean to replace
-  a * b with b * a in the goal. -/
+  a * b with b * a in the goal. Now the goal becomes (b * a) * c = 
+  b * (a * c). -/
   
   rw mul_assoc b a c,
   /- mul_assoc b a c refers to the associative law for multiplication 
   on b, a, and c: b * a * c = b * (a * c). Note that Lean leaves out 
-  parentheses when possible. Since multiplication associates to the left,
+  parentheses when possible. Since multiplication is left-associative,
   the left-hand side of mul_assoc b * a * c refers to (b * a) * c. 
   'rw mul_assoc b a c' solves the goal b * a * c = b * (a * c), and hence, 
   completes the proof. -/
