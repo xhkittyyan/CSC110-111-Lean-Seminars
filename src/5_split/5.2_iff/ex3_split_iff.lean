194dc6a8035c7de37b6456726e324e28335041de
xhkittyyan <xh.yan@utoronto.ca>
2022-08-25 12:31:20 -0400

    Update ex3_split_iff.lean

--- src/5_split/5.2_iff/ex3_split_iff.lean
+++ src/5_split/5.2_iff/ex3_split_iff.lean
@@ -5,13 +5,30 @@ example {x y : ℝ} (h : x ≤ y) : ¬ y ≤ x ↔ x ≠ y :=
 begin
   split,
     intro h1,
-    push_neg at h1,
-    sorry,
+    intro h2,
+    rw h2 at h1,
+    rw h2 at h,
+    exact h1 h,
   intro h1,
   contrapose! h1,
   exact le_antisymm h h1,
 end
 
+-- Alternatively
+example {x y : ℝ} (h : x ≤ y) : ¬ y ≤ x ↔ x ≠ y :=
+begin
+  split,
+    intro h1,
+    push_neg at h1,
+    intro h2,
+    rw h2 at h1,
+    exact (lt_irrefl y) h1,
+  intro h1,
+  contrapose! h1,
+  exact le_antisymm h h1,
+end
+
+-- Alternatively
 example {x y : ℝ} (h : x ≤ y) : ¬ y ≤ x ↔ x ≠ y :=
 begin
   split,
