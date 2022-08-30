679d2b038be6bd395a2ec0a182bc874d8563da8e
xhkittyyan <xh.yan@utoronto.ca>
2022-08-30 14:18:36 -0400

    updates

--- src/4_cases/4.3_cases_exist/ex9_cases_dvd_a(b+c).lean
+++ src/4_cases/4.3_cases_exist/ex9_cases_dvd_a(b+c).lean
@@ -1,5 +1,8 @@
 import tactic
 
+#check mul_add
+#check dvd_mul_right
+
 variables {a b c : ℕ}
 
 -- BEGIN
@@ -7,7 +10,16 @@ example (divab : a ∣ b) (divac : a ∣ c) : a ∣ (b + c) :=
 begin 
   cases divab with d beq,
   cases divac with e ceq,
-  rw [ceq, beq],
+  rw [beq, ceq],
+  rw ← mul_add,
+  exact dvd_mul_right a (d + e),
+end
+
+example (divab : a ∣ b) (divac : a ∣ c) : a ∣ (b + c) :=
+begin 
+  cases divab with d beq,
+  cases divac with e ceq,
+  rw [beq, ceq],
   rw ← mul_add,
   use (d + e),
 end
