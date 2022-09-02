ce02b12db5d6ff51b948a12ad290ffb3fd0fc7a9
Ruotong Cheng <chrt2000@gmail.com>
2022-09-02 15:34:14 -0400

    Fix the proof of the triangle sum

--- src/10_Induction/ex4_induction_triangle_sum.lean
+++ src/10_Induction/ex4_induction_triangle_sum.lean
@@ -1,8 +1,8 @@
-import data.real.basic
+import tactic
+open_locale big_operators
+open finset
 
-open nat 
-
-theorem sum_id (n : ℕ) : Σ i in range (n + 1), i = n * (n + 1) / 2 :=
+example (n : ℕ) : ∑ i in range (n + 1), i = n * (n + 1) / 2 :=
 begin
   symmetry, 
   apply nat.div_eq_of_eq_mul_right (by norm_num : 0 < 2),
@@ -12,16 +12,13 @@ begin
   ring,
 end
 
-example (n : ℕ) : 2 * triangle_sum n = n * (n+1) :=
-begin 
-  induction n with k h_ih,
-  { unfold triangle_sum,
-    rw sum_range_one,
-    norm_num, },
-  { unfold triangle_sum at *,
-    rw sum_range_succ,
+example (n : ℕ) : 2 * ∑ j in range (n + 1), j = n * (n + 1) :=
+begin
+  induction n with k h,
+  { simp },
+  { rw sum_range_succ,
     rw mul_add,
-    rw h_ih,
-    rw succ_eq_add_one,
-    ring, },
-end 
+    rw h,
+    rw nat.succ_eq_add_one,
+    ring },
+end
