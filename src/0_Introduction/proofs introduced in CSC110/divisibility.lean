8aee53a03be0b664935d2637a14eae2bb218b196
Ruotong Cheng <chrt2000@gmail.com>
2022-09-27 18:23:10 -0400

    Update the proofs about divisibility

--- src/0_Introduction/proofs introduced in CSC110/divisibility.lean
+++ src/0_Introduction/proofs introduced in CSC110/divisibility.lean
@@ -1,33 +1,27 @@
 import tactic 
 open nat 
 
-variables {n d t : ℕ} 
+variables {n d : ℤ} 
 #check one_mul
 #check @le_self_mul 
 
-example (n : ℕ) : (1 ∣ n) :=
+example : (1 ∣ n) :=
 begin
-  cases n with d hd,
-  { norm_num,},
-  { use d + 1,
-  rw one_mul, }
-end 
-
-example : ∀ n d : ℤ, ∃ k : ℤ, n = d * k → d ≤ n := 
-begin
-  intros n d,
-  use 1,
-  intro hdn,
-  rw hdn,
-  rw mul_one,
+  use n,
+  rw one_mul,
 end
 
-example : ∀ n d : ℤ, d ∣ n → d ≤ n := 
+example (hn : n > 0) (hd : d > 0): d ∣ n → d ≤ n := 
 begin
-  intros n d,
   intro hdn,
-  cases hdn with t ht,
-  rw ht,
-  exact le_self_mul,
-end 
-
+  rcases hdn with ⟨k, rfl⟩,
+  nth_rewrite_lhs 0 ← mul_one d,
+  apply mul_le_mul,
+  exact le_refl d,
+  {
+    rcases lt_trichotomy k 0 with hk | rfl | hk;
+    nlinarith,
+  },
+  exact zero_le_one,
+  exact le_of_lt hd,
+end
